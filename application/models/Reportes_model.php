3b40b09c7fe51828a27f06a92dbc3b11fdf689d5
cristian <cristian@Vidal-PC>
2016-11-13 21:58:36 -0300

    AVANCES TESIS  13-11-2016

--- application/models/Reportes_model.php
+++ application/models/Reportes_model.php
@@ -8,22 +8,21 @@ class Reportes_model extends CI_Model
     }  
     
     public function consolidado_datos_paciente($rut){
-        $query = $this->db->query("SELECT 
-                                        *  
+        $query = $this->db->query("SELECT *  
                                     FROM tbl_usuarios tu 
                                     JOIN tbl_historias_medicas thm on thm.id_paciente = tu.id_usuario
-                                    JOIN tbl_estado_civil tec ON tec.id_estado_civil = tu.id_estado_civil
-                                    JOIN tbl_ocupaciones tbo on tbo.cod_ocupacion = tu.id_ocupacion
-                                    JOIN tbl_region tr on tr.region_id = tu.id_region
-                                    JOIN tbl_comuna tc on tc.COMUNA_ID = tu.id_comuna
-                                    JOIN tbl_provincia tpro on tpro.PROVINCIA_ID = tu.id_provincia
-                                    JOIN tbl_paises tp on tp.cod_pais = tu.nacionalidad
-                                    JOIN tbl_ocupaciones toc on toc.cod_ocupacion = tu.id_ocupacion
-                                    JOIN tbl_previsiones_medicas tpm on tpm.id_prevision_medica = tu.id_prevision
-                                    JOIN tbl_religiones tre on tre.id_religion = tu.id_religion
-                                    JOIN tbl_niveles_estudios tne on tne.id_nivel_estudio = tu.id_nivel_estudio
-                                    JOIN tbl_factores_rh tfrh on tfrh.id_factor_rh = tu.id_factorn_rh
-                                    JOIN tbl_grupos_sanguineos tgs on tgs.id_grupo_sanguineo = tu.id_grupo_sang
+                                    LEFT JOIN tbl_estado_civil tec ON tec.id_estado_civil = tu.id_estado_civil
+                                    LEFT JOIN tbl_ocupaciones tbo on tbo.cod_ocupacion = tu.id_ocupacion
+                                    LEFT JOIN tbl_region tr on tr.region_id = tu.id_region
+                                    LEFT JOIN tbl_comuna tc on tc.COMUNA_ID = tu.id_comuna
+                                    LEFT JOIN tbl_provincia tpro on tpro.PROVINCIA_ID = tu.id_provincia
+                                    LEFT JOIN tbl_paises tp on tp.cod_pais = tu.nacionalidad
+                                    LEFT JOIN tbl_ocupaciones toc on toc.cod_ocupacion = tu.id_ocupacion
+                                    LEFT JOIN tbl_previsiones_medicas tpm on tpm.id_prevision_medica = tu.id_prevision
+                                    LEFT JOIN tbl_religiones tre on tre.id_religion = tu.id_religion
+                                    LEFT JOIN tbl_niveles_estudios tne on tne.id_nivel_estudio = tu.id_nivel_estudio
+                                    LEFT JOIN tbl_factores_rh tfrh on tfrh.id_factor_rh = tu.id_factorn_rh
+                                    LEFT JOIN tbl_grupos_sanguineos tgs on tgs.id_grupo_sanguineo = tu.id_grupo_sang
                                     WHERE replace(replace(rut, '.', ''), '-', '') = '" . $rut ."'" );
         $r = $query->result_array();            
         $data = array();
